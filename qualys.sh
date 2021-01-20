9a6223e4105cc03490aa6189086240b34321de5b
xD3ath <anvar.sadikhov@outlook.com>
2021-01-20 17:21:51 +0400

    jsonFormatting

--- qualys.sh
+++ qualys.sh
@@ -12,16 +12,13 @@ function auth_func() {
   pWord=$(cat auth | awk '{print $2}')
 
   auth_token=$(curl -X POST 'https://gateway.qg2.apps.qualys.eu/auth' -d 'username='"$uName"'&password='"$pWord"'&token=true' -H 'ContentType:application/x-www-form-urlencoded')
-
 }
 
 ## Function for checking if JSON is correct. True/False/FileNotFoundError
 
 function json_checker() {
-  parser_message=$(echo "$jsonPath" | python3 -c "
-
-import os
-import json
+  parser_message=$(echo "$json_path" | python3 -c "
+import os, json
 from json import JSONDecodeError
 
 try:
@@ -33,7 +30,6 @@ except FileNotFoundError as e:
 
 except JSONDecodeError as ex:
     print("False")
-
     ")
 }
 
@@ -48,7 +44,7 @@ count_post="curl -X POST -H 'Accept: */*' -H 'Authorization: Bearer "$auth_token
 
 eval "$count_post" >count.json
 
-jsonPath="count.json"
+json_path="count.json"
 json_checker
 
 while [ "$parser_message" == "False" ]; do
@@ -60,21 +56,19 @@ while [ "$parser_message" == "False" ]; do
 done
 
 count_parse=$(echo "count.json" | python3 -c "
-
-import os
-import json
+import os, json
 
 data = json.load(open('count.json'))
 num = int(data['count'])
+
 print(num)
+    ")
 
-")
-
-json_files_num=$((($count_parse / 100) + (100 % 3 > 0)))
+json_nums=$((($count_parse / 100) + (100 % 3 > 0)))
 
 # Output count of all assets in count.log and CLI
 echo "Count of all assets for ""$timeStart"" is:" "$count_parse" >logs/count.log
-echo "Number of JSON files after generate:" $json_files_num >>logs/count.log
+echo "Number of JSON files after generate:" $json_nums >>logs/count.log
 cat logs/count.log
 
 ### For generating and downloading of the First JSON file
@@ -84,7 +78,7 @@ firstFile_post="curl -X POST -H 'Accept: */*' -H 'Authorization: Bearer "$auth_t
 echo "1"
 eval "$firstFile_post" >content/content1.json
 
-jsonPath="content/content1.json"
+json_path="content/content1.json"
 json_checker
 
 while [ "$parser_message" == "False" ]; do
@@ -99,10 +93,8 @@ done
 # Check lastSeenAssetId if exists
 
 function generate_nextJson() {
-  lastId_parser=$(echo "$jsonFile" | python3 -c "
-
-import os
-import json
+  lastId_parser=$(echo "$json_file" | python3 -c "
+import os, json
 
 with open(input()) as f:
 
@@ -111,15 +103,12 @@ with open(input()) as f:
 
 last_id = int(cont_list['lastSeenAssetId'])
 print(last_id)
-
     ")
 
   echo "lastSeenAssetId is:" "$lastId_parser"
 
   if [ ! -z "$lastId_parser" ]; then
-
     nextFile_post="curl -X POST -H 'Accept: */*' -H 'Authorization: Bearer "$auth_token"' -H 'Content-Type: application/json'  'https://gateway.qg2.apps.qualys.eu/am/v1/assets/host/list?lastSeenAssetId="$lastId_parser"'"
-
   else
     echo "Request failed"
     exit
@@ -129,12 +118,12 @@ print(last_id)
 ### Function for downloading of the Next JSON file
 
 function download_json() {
-  jsonFile="content/content$prevNum.json"
+  json_file="content/content$prevNum.json"
   generate_nextJson
 
   eval "$nextFile_post" >content/content"$num".json
 
-  jsonPath="content/content$num.json"
+  json_path="content/content$num.json"
   json_checker
 
   while [ "$parser_message" == "False" ]; do
@@ -148,7 +137,7 @@ function download_json() {
 
 ### Generate JSON files in loop
 
-for ((n = 2; n <= $json_files_num; n++)); do
+for ((n = 2; n <= $json_nums; n++)); do
   echo "$n"
   prevNum=$((n - 1))
   num=$n
